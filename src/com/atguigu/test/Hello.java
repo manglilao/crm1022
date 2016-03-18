f1bfe64872d743799f8aa48b0ba9a01c15e529b4
tang <2509433646@qq.com>
2016-03-18 21:49:32 +0800

    update hello

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -3,6 +3,7 @@ package com.atguigu.test;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("hello");
-	System.out.println("hello");
+	System.out.println("hello2");
+	System.out.println("hello2");
 }
 }
